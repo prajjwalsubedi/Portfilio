2ae98c6b9683416fd7c567c45655dca2c5d97949
Prajjwal Subedi <prajjwalsubedi95@gmail.com>
2024-08-08 18:11:08 -0400

    test update

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 
 import Header from './components/Header';
 import About from './components/About';
-import Service from './components/Service';
-import Skills from './components/Skills';
-import Experience from './components/Experience';
-import Testmonial from './components/Testmonial';
-import Blog from './components/Blog';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+// import Service from './components/Service';
+// import Skills from './components/Skills';
+// import Experience from './components/Experience';
+// import Testmonial from './components/Testmonial';
+// import Blog from './components/Blog';
+// import Contact from './components/Contact';
+// import Footer from './components/Footer';
 
 
 import reportWebVitals from './reportWebVitals';
@@ -20,13 +20,13 @@ ReactDOM.render(
   <React.StrictMode>
     <Header />
     <About />
-    <Service />
+    {/* <Service />
     <Skills />
     <Experience />
     <Testmonial />
     <Blog />
     <Contact />
-    <Footer />
+    <Footer /> */}
   </React.StrictMode>,
 
   document.getElementById('root')

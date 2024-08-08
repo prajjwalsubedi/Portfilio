9a41ab08b4939e54276031d9b23dee4c6f44b77b
Prajjwal Subedi <prajjwalsubedi95@gmail.com>
2024-08-08 16:34:41 -0400

    revert

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Header';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );

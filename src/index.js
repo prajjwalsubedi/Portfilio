c2da64739a6ff67ca182e6aff52579a5666aa513
Prajjwal Subedi <prajjwalsubedi95@gmail.com>
2024-08-08 16:46:11 -0400

    header Update

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './Component/Header';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Header />
   </React.StrictMode>,
   document.getElementById('root')
 );

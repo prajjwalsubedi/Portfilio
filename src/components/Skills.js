34bde2cb6cbc82e9313f52f9258f58bd553c5989
Prajjwal Subedi <prajjwalsubedi95@gmail.com>
2024-08-08 18:31:14 -0400

    Message

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -14,63 +14,63 @@ function Skills() {
                         <div class="col-sm-6">
                             <h6 class="mb-3">Test Automation</h6>
                             <div class="progress">
-                                <div class="progress-bar bg-primary" role="progressbar" style="width: 92%;" aria-valuenow="25"
+                                <div class="progress-bar bg-primary" role="progressbar" style={{width: "92%"}} aria-valuenow="25"
                                     aria-valuemin="0" aria-valuemax="100"><span>92%</span></div>
                             </div>
                         </div>
                         <div class="col-sm-6">
                             <h6 class="mb-3">API Testing</h6>
                             <div class="progress">
-                                <div class="progress-bar bg-primary" role="progressbar" style="width: 89%;" aria-valuenow="25"
+                                <div class="progress-bar bg-primary" role="progressbar" style={{width: "89%"}} aria-valuenow="25"
                                     aria-valuemin="0" aria-valuemax="100"><span>89%</span></div>
                             </div>
                         </div>
                         <div class="col-sm-6">
                             <h6 class="mb-3">Manual Testing</h6>
                             <div class="progress">
-                                <div class="progress-bar bg-primary" role="progressbar" style="width: 88%;" aria-valuenow="25"
+                                <div class="progress-bar bg-primary" role="progressbar" style={{width: "88%"}} aria-valuenow="25"
                                     aria-valuemin="0" aria-valuemax="100"><span>88%</span></div>
                             </div>
                         </div>
                         <div class="col-sm-6">
                             <h6 class="mb-3">Test Automation Frameworks</h6>
                             <div class="progress">
-                                <div class="progress-bar bg-primary" role="progressbar" style="width: 90%;" aria-valuenow="25"
+                                <div class="progress-bar bg-primary" role="progressbar" style={{width: "90%"}} aria-valuenow="25"
                                     aria-valuemin="0" aria-valuemax="100"><span>90%</span></div>
                             </div>
                         </div>
                         <div class="col-sm-6">
                             <h6 class="mb-3">CI/CD Integration</h6>
                             <div class="progress">
-                                <div class="progress-bar bg-primary" role="progressbar" style="width: 85%;" aria-valuenow="25"
+                                <div class="progress-bar bg-primary" role="progressbar" style={{width: "85%"}} aria-valuenow="25"
                                     aria-valuemin="0" aria-valuemax="100"><span>85%</span></div>
                             </div>
                         </div>
                         <div class="col-sm-6">
                             <h6 class="mb-3">Selenium WebDriver</h6>
                             <div class="progress">
-                                <div class="progress-bar bg-primary" role="progressbar" style="width: 91%;" aria-valuenow="25"
+                                <div class="progress-bar bg-primary" role="progressbar" style={{width: "91%"}} aria-valuenow="25"
                                     aria-valuemin="0" aria-valuemax="100"><span>91%</span></div>
                             </div>
                         </div>
                         <div class="col-sm-6">
                             <h6 class="mb-3">JIRA</h6>
                             <div class="progress">
-                                <div class="progress-bar bg-primary" role="progressbar" style="width: 87%;" aria-valuenow="25"
+                                <div class="progress-bar bg-primary" role="progressbar" style={{width: "87%"}} aria-valuenow="25"
                                     aria-valuemin="0" aria-valuemax="100"><span>87%</span></div>
                             </div>
                         </div>
                         <div class="col-sm-6">
                             <h6 class="mb-3">API Test Tools - Postman</h6>
                             <div class="progress">
-                                <div class="progress-bar bg-primary" role="progressbar" style="width: 70%;" aria-valuenow="25"
+                                <div class="progress-bar bg-primary" role="progressbar" style={{width: "70%"}} aria-valuenow="25"
                                     aria-valuemin="0" aria-valuemax="100"><span>70%</span></div>
                             </div>
                         </div>
                         <div class="col-sm-6">
                             <h6 class="mb-3">BDD Frameworks - Cucumber</h6>
                             <div class="progress">
-                                <div class="progress-bar bg-primary" role="progressbar" style="width: 65%;" aria-valuenow="25"
+                                <div class="progress-bar bg-primary" role="progressbar" style={{width: "65%"}} aria-valuenow="25"
                                     aria-valuemin="0" aria-valuemax="100"><span>65%</span></div>
                             </div>
                         </div>
@@ -84,4 +84,3 @@ function Skills() {
   }
   
   export default Skills;
-  

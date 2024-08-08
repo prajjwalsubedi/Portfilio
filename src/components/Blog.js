a043f75bffb1ff27ef80b89194baf4210dfffd02
Prajjwal Subedi <prajjwalsubedi95@gmail.com>
2024-08-08 19:20:20 -0400

    Updates

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -4,57 +4,24 @@ function Blog() {
             {/* <!-- Blog Section --> */}
             <section id="blog" class="section">
                 <div class="container text-center">
-                    <h6 class="subtitle">My Blogs</h6>
-                    <h6 class="section-title mb-4">Latest News</h6>
-                    <p class="mb-5 pb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. In alias dignissimos. <br />
-                        rerum commodi corrupti, temporibus non quam.</p>
-
+                <h6 class="subtitle">My Blogs</h6>
+                <h6 class="section-title mb-4">Recent Posts</h6>
+                <p class="mb-5 pb-4">Explore insightful articles on the latest trends, best practices, and expert tips in QA testing. Stay updated with the evolving world of software quality assurance, and enhance your testing knowledge. <br />
+                    Dive into topics that matter most to QA professionals.</p>
                     <div class="row text-left">
                         <div class="col-md-4">
                             <div class="card border mb-4">
-                                <img src="assets/imgs/blog-1.jpg" alt="" class="card-img-top w-100"/>
+                                <img src="assets/imgs/blog/The_Evolution_of_QA.png" alt="" class="card-img-top w-100"/>
                                 <div class="card-body">
-                                    <h5 class="card-title">Designe for Everyone</h5>
+                                    <h5 class="card-title">The Evolution of QA: From Manual Testing to Automation</h5>
                                     <div class="post-details">
-                                        <a href="javascript:void(0)">Posted By: Admin</a>
-                                        <a href="javascript:void(0)"><i class="ti-thumb-up"></i> 456</a>
-                                        <a href="javascript:void(0)"><i class="ti-comment"></i> 123</a>
+                                        <a href="javascript:void(0)">Posted By: Prajjwal</a>
+                                        {/* <a href="javascript:void(0)"><i class="ti-thumb-up"></i> 456</a>
+                                        <a href="javascript:void(0)"><i class="ti-comment"></i> 123</a> */}
                                     </div>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut ad vel dolorum, iusto velit,
-                                        minima? Voluptas nemo harum impedit nisi.</p>
-                                    <a href="javascript:void(0)">Read More..</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card border mb-4">
-                                <img src="assets/imgs/blog-2.jpg" alt="" class="card-img-top w-100"/>
-                                <div class="card-body">
-                                    <h5 class="card-title">Web Layouts</h5>
-                                    <div class="post-details">
-                                        <a href="javascript:void(0)">Posted By: Admin</a>
-                                        <a href="javascript:void(0)"><i class="ti-thumb-up"></i> 456</a>
-                                        <a href="javascript:void(0)"><i class="ti-comment"></i> 123</a>
-                                    </div>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut ad vel dolorum, iusto velit,
-                                        minima? Voluptas nemo harum impedit nisi.</p>
-                                    <a href="javascript:void(0)">Read More..</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card border mb-4">
-                                <img src="assets/imgs/blog-3.jpg" alt="" class="card-img-top w-100"/>
-                                <div class="card-body">
-                                    <h5 class="card-title">Bootstrap Framework</h5>
-                                    <div class="post-details">
-                                        <a href="javascript:void(0)">Posted By: Admin</a>
-                                        <a href="javascript:void(0)"><i class="ti-thumb-up"></i> 456</a>
-                                        <a href="javascript:void(0)"><i class="ti-comment"></i> 123</a>
-                                    </div>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut ad vel dolorum, iusto velit,
-                                        minima? Voluptas nemo harum impedit nisi.</p>
-                                    <a href="javascript:void(0)">Read More..</a>
+                                    <p>
+                                    QA has transformed from manual testing to advanced automation, improving speed, scalability, and accuracy. With AI and continuous integration, QA now plays a crucial role in delivering high-quality software rapidly and efficiently.</p>
+                                    <a href="https://www.linkedin.com/pulse/evolution-qa-from-manual-testing-automation-prajjwal-subedi-pcqge">Read More..</a>
                                 </div>
                             </div>
                         </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,11 +8,11 @@ function Contact() {
                         <div class="card-body text-light">
                             <div class="row align-items-center">
                                 <div class="col-sm-9 text-center text-sm-left">
-                                    <h5 class="mt-3">Hire Me For Your Project</h5>
-                                    <p class="mb-3">Accusantium labore nostrum similique quisquam.</p>
+                                <h5 class="mt-3">Hire Me For Your Project</h5>
+                                <p class="mb-3">Looking for a skilled QA professional to ensure the quality of your software? Let's collaborate to deliver exceptional results and elevate your project to the next level.</p>
                                 </div>
                                 <div class="col-sm-3 text-center text-sm-right">
-                                    <button class="btn btn-light rounded">Hire Me!</button>
+                                    <button class="btn btn-light rounded" href="mailto:email@email.de">Hire Me!</button>
                                 </div>
                             </div>
                         </div>
@@ -24,14 +24,13 @@ function Contact() {
             {/* <!-- Contact Section --> */}
             <section id="contact" class="position-relative section">
                 <div class="container text-center">
-                    <h6 class="subtitle">Contact</h6>
-                    <h6 class="section-title mb-4">Get In Touch With Me</h6>
-                    <p class="mb-5 pb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. In alias dignissimos. <br />
-                        rerum commodi corrupti, temporibus non quam.</p>
-
+                <h6 class="subtitle">Contact</h6>
+                <h6 class="section-title mb-4">Get In Touch</h6>
+                <p class="mb-5 pb-4">I'd love to hear from you! Whether you have a question, need assistance, or want to discuss a project, feel free to reach out. <br />
+                    Let's connect and explore how we can work together.</p>
                     <div class="contact text-left">
                         <div class="form">
-                            <h6 class="subtitle">Available 24/7</h6>
+                            <h6 class="subtitle">Available on Office hours</h6>
                             <h6 class="section-title mb-4">Get In Touch</h6>
                             <form>
                                 <div class="form-group">
@@ -39,8 +38,8 @@ function Contact() {
                                         aria-describedby="emailHelp" placeholder="Enter email" required/>
                                 </div>
                                 <div class="form-group">
-                                    <input type="password" class="form-control" id="exampleInputPassword1"
-                                        placeholder="Password" required/>
+                                    <input type="text" class="form-control" id="exampleInputSubject"
+                                        placeholder="Subject" required/>
                                 </div>
                                 <div class="form-group">
                                     <textarea name="contact-message" id="" cols="30" rows="5" class="form-control"
@@ -54,21 +53,21 @@ function Contact() {
                                 <i class="ti-location-pin"></i>
                                 <div class="">
                                     <h5>Location</h5>
-                                    <p> 12345 Fake ST NoWhere AB Country</p>
+                                    <p>Broad Run, Virginia, USA</p>
                                 </div>
                             </div>
                             <div class="item">
                                 <i class="ti-mobile"></i>
                                 <div>
                                     <h5>Phone Number</h5>
-                                    <p>(123) 456-7890</p>
+                                    <p>(571) 538-9808</p>
                                 </div>
                             </div>
                             <div class="item">
                                 <i class="ti-email"></i>
                                 <div class="mb-0">
                                     <h5>Email Address</h5>
-                                    <p>info@website.com</p>
+                                    <p>prajjwalsubedi95@gmail.com</p>
                                 </div>
                             </div>
                         </div>

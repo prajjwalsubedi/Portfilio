f6707635b85bcba3e4aaa16a651e7bc771995eaf
Prajjwal Subedi <prajjwalsubedi95@gmail.com>
2024-08-09 01:45:21 -0400

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -72,7 +72,7 @@ function Contact() {
                                     <textarea name="message" id="exampleInputMessage" cols="30" rows="5" 
                                         class="form-control" placeholder="Message" required></textarea>
                                 </div>
-                                <button type="submit" class="btn btn-primary btn-block rounded w-lg">Send Message</button>
+                                <button type="submit" class="btn btn-primary btn-block rounded w-lg" value="Send">Send Message</button>
                             </form>
                         </div>
                         <div class="contact-infos">
